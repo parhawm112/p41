20d8b9eafc84b828008fcd49ea278bcc55d6076b
parhawm112 <117488273+parhawm112@users.noreply.github.com>
2023-01-14 11:37:18 -0800

    Add files via upload

---
+++ p41.main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	float x,y,s,p,z;
+	
+	cout<<"x";
+	cin>>x;
+	cout<<"y";
+	cin>>y;
+	
+	s=x+y;
+	p=x*y;
+	z=x-y;
+	cout<<"s=x+y"<<"/n"<<"p=x*y"<<"\n"<<"z=x-y";
+return 0;
+
+}
